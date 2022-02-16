956e08910d5c360bad834ac3c2d459d43bd64954
Adam Hartford <adamhartford@outlook.com>
2022-02-16 13:26:50 -0500

    Initial commit

---
+++ SiriusXM/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using AppKit;
+using Foundation;
+
+namespace SiriusXM
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+
+        [Export("applicationShouldHandleReopen:hasVisibleWindows:")]
+        public override bool ApplicationShouldHandleReopen(NSApplication sender, bool hasVisibleWindows)
+        {
+            if (!hasVisibleWindows)
+            {
+                foreach (var window in sender.DangerousWindows)
+                {
+                    window.MakeKeyAndOrderFront(this);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SiriusXM/ViewController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+using WebKit;
+
+namespace SiriusXM
+{
+    public partial class ViewController : NSViewController, IWKNavigationDelegate, IWKUIDelegate
+    {
+        private WKWebView _browser;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var config = new WKWebViewConfiguration();
+            config.Preferences.SetValueForKey(NSObject.FromObject(true), new NSString("developerExtrasEnabled"));
+
+            _browser = new WKWebView(View.Frame, config);
+            _browser.AutoresizingMask = NSViewResizingMask.HeightSizable | NSViewResizingMask.WidthSizable;
+            _browser.NavigationDelegate = this;
+            _browser.UIDelegate = this;
+
+            // Using Edge for Mac user agent because Safari user agent doesn't work here with SiriusXM player.
+            _browser.CustomUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36 Edg/98.0.1108.51";
+
+            _browser.LoadRequest(new NSUrlRequest(new NSUrl("https://player.siriusxm.com")));
+            View.AddSubview(_browser);
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
